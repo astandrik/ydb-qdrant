6fe3003e6f67776354d23ff996d81ec2b6abc854
github-action-benchmark <github@users.noreply.github.com>
2025-12-02 20:34:09 +0000

    add Load Test - Stress Capacity (one_table, 768D) (customBiggerIsBetter) benchmark result for 5736d259bffe4533808880c7cada2f0f96a2c574

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1764707647734,
+  "lastUpdate": 1764707649252,
   "repoUrl": "https://github.com/astandrik/ydb-qdrant",
   "entries": {
     "Load Test - Soak (multi_table, 768D)": [
@@ -1969,6 +1969,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "Stress: Throughput",
             "value": 112.0856002655837,
+            "unit": "ops/s"
+          },
+          {
+            "name": "Stress: Max VUs",
+            "value": 600,
+            "unit": "VUs"
+          },
+          {
+            "name": "Stress: Breaking Point VUs",
+            "value": 600,
+            "unit": "VUs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "astandrik@yandex-team.ru",
+            "name": "Anton Standrik",
+            "username": "astandrik"
+          },
+          "committer": {
+            "email": "astandrik@yandex-team.ru",
+            "name": "Anton Standrik",
+            "username": "astandrik"
+          },
+          "distinct": true,
+          "id": "5736d259bffe4533808880c7cada2f0f96a2c574",
+          "message": "fix: better charts",
+          "timestamp": "2025-12-02T23:26:01+03:00",
+          "tree_id": "e67732e72d81ee25dbea31632572ebdb3540eb54",
+          "url": "https://github.com/astandrik/ydb-qdrant/commit/5736d259bffe4533808880c7cada2f0f96a2c574"
+        },
+        "date": 1764707648627,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Stress: Throughput",
+            "value": 127.03258172289347,
             "unit": "ops/s"
           },
           {
