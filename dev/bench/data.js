d220127fae80b957c827624250f3641424c3a5c7
github-action-benchmark <github@users.noreply.github.com>
2025-12-02 21:26:32 +0000

    add Load Test - Stress (one_table, 768D) (customSmallerIsBetter) benchmark result for d21d16d6380fdccc5f2d3f202491b5431470ba01

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1764710790968,
+  "lastUpdate": 1764710792202,
   "repoUrl": "https://github.com/astandrik/ydb-qdrant",
   "entries": {
     "Load Test - Soak (multi_table, 768D)": [
@@ -2865,6 +2865,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Stress: Upsert Latency p95",
             "value": 4961.299999999999,
+            "unit": "ms"
+          },
+          {
+            "name": "Stress: Error Rate",
+            "value": 0,
+            "unit": "%"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "astandrik@yandex-team.ru",
+            "name": "Anton Standrik",
+            "username": "astandrik"
+          },
+          "committer": {
+            "email": "astandrik@yandex-team.ru",
+            "name": "Anton Standrik",
+            "username": "astandrik"
+          },
+          "distinct": true,
+          "id": "d21d16d6380fdccc5f2d3f202491b5431470ba01",
+          "message": "fix: smaller charts",
+          "timestamp": "2025-12-03T00:18:27+03:00",
+          "tree_id": "7215e9a6e6e7ef5b7e59af8b4d0726c225830d6a",
+          "url": "https://github.com/astandrik/ydb-qdrant/commit/d21d16d6380fdccc5f2d3f202491b5431470ba01"
+        },
+        "date": 1764710791900,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Stress: Search Latency p95",
+            "value": 5598,
+            "unit": "ms"
+          },
+          {
+            "name": "Stress: Search Latency p99",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "Stress: Search Latency max",
+            "value": 5728,
+            "unit": "ms"
+          },
+          {
+            "name": "Stress: Upsert Latency p95",
+            "value": 5438.25,
             "unit": "ms"
           },
           {
