17c2c33d6227cd999d5babf5fe0afd1db6857962
github-action-benchmark <github@users.noreply.github.com>
2025-12-02 21:22:25 +0000

    add Load Test - Soak Capacity (one_table, 768D) (customBiggerIsBetter) benchmark result for d21d16d6380fdccc5f2d3f202491b5431470ba01

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1764710544066,
+  "lastUpdate": 1764710545105,
   "repoUrl": "https://github.com/astandrik/ydb-qdrant",
   "entries": {
     "Load Test - Soak (multi_table, 768D)": [
@@ -1582,6 +1582,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Soak: Throughput",
             "value": 48.28358600850335,
+            "unit": "ops/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "astandrik@yandex-team.ru",
+            "name": "Anton Standrik",
+            "username": "astandrik"
+          },
+          "committer": {
+            "email": "astandrik@yandex-team.ru",
+            "name": "Anton Standrik",
+            "username": "astandrik"
+          },
+          "distinct": true,
+          "id": "d21d16d6380fdccc5f2d3f202491b5431470ba01",
+          "message": "fix: smaller charts",
+          "timestamp": "2025-12-03T00:18:27+03:00",
+          "tree_id": "7215e9a6e6e7ef5b7e59af8b4d0726c225830d6a",
+          "url": "https://github.com/astandrik/ydb-qdrant/commit/d21d16d6380fdccc5f2d3f202491b5431470ba01"
+        },
+        "date": 1764710544879,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Soak: Throughput",
+            "value": 52.16666289551294,
             "unit": "ops/s"
           }
         ]
