86227e073ad6e54bd055aa0f11530fd593a90de2
github-action-benchmark <github@users.noreply.github.com>
2025-12-03 06:50:06 +0000

    add Load Test - Stress (multi_table, 768D) (customSmallerIsBetter) benchmark result for 046918324e440e607580a3eb5e33b80825a98eb5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1764744382320,
+  "lastUpdate": 1764744605993,
   "repoUrl": "https://github.com/astandrik/ydb-qdrant",
   "entries": {
     "Load Test - Soak (multi_table, 768D)": [
@@ -2219,6 +2219,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Stress: Upsert Latency p95",
             "value": 2626.5999999999995,
+            "unit": "ms"
+          },
+          {
+            "name": "Stress: Error Rate",
+            "value": 0,
+            "unit": "%"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "astandrik@yandex-team.ru",
+            "name": "Anton Standrik",
+            "username": "astandrik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "046918324e440e607580a3eb5e33b80825a98eb5",
+          "message": "chore(main): release 4.7.0 (#114)",
+          "timestamp": "2025-12-03T09:42:12+03:00",
+          "tree_id": "dc70c6818dc056baa7964a4b9a54280bcead3c12",
+          "url": "https://github.com/astandrik/ydb-qdrant/commit/046918324e440e607580a3eb5e33b80825a98eb5"
+        },
+        "date": 1764744605489,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Stress: Search Latency p95",
+            "value": 3731,
+            "unit": "ms"
+          },
+          {
+            "name": "Stress: Search Latency p99",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "Stress: Search Latency max",
+            "value": 3858,
+            "unit": "ms"
+          },
+          {
+            "name": "Stress: Upsert Latency p95",
+            "value": 2700,
             "unit": "ms"
           },
           {
