5a5e15dee466670c5236376b9448ca9edf082ca1
github-action-benchmark <github@users.noreply.github.com>
2025-12-03 07:57:51 +0000

    add Load Test - Soak (one_table, 768D) (customSmallerIsBetter) benchmark result for 51cd975fc53abcd5e529dc23bd13ae120d4c932b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1764748668843,
+  "lastUpdate": 1764748671437,
   "repoUrl": "https://github.com/astandrik/ydb-qdrant",
   "entries": {
     "Load Test - Soak (multi_table, 768D)": [
@@ -1447,6 +1447,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "Soak: Upsert Latency p95",
             "value": 63,
+            "unit": "ms"
+          },
+          {
+            "name": "Soak: Error Rate",
+            "value": 0,
+            "unit": "%"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "astandrik@yandex-team.ru",
+            "name": "Anton Standrik",
+            "username": "astandrik"
+          },
+          "committer": {
+            "email": "astandrik@yandex-team.ru",
+            "name": "Anton Standrik",
+            "username": "astandrik"
+          },
+          "distinct": true,
+          "id": "51cd975fc53abcd5e529dc23bd13ae120d4c932b",
+          "message": "fix: build for linux amd64",
+          "timestamp": "2025-12-03T10:53:54+03:00",
+          "tree_id": "9af7dc8ec763d540f2ff103fda20bb2c0f579eec",
+          "url": "https://github.com/astandrik/ydb-qdrant/commit/51cd975fc53abcd5e529dc23bd13ae120d4c932b"
+        },
+        "date": 1764748670154,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Soak: Search Latency p95",
+            "value": 123,
+            "unit": "ms"
+          },
+          {
+            "name": "Soak: Search Latency p99",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "Soak: Upsert Latency p95",
+            "value": 57,
             "unit": "ms"
           },
           {
