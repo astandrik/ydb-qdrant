089780b3299a955a71bd415fa0227d38996f9229
github-action-benchmark <github@users.noreply.github.com>
2025-12-05 14:54:13 +0000

    add Load Test - Stress (one_table, 768D) (customSmallerIsBetter) benchmark result for 39d8d1c6231f133692e7b574de54f783f3778ecd

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1764946451850,
+  "lastUpdate": 1764946453144,
   "repoUrl": "https://github.com/astandrik/ydb-qdrant",
   "entries": {
     "Load Test - Soak (multi_table, 768D)": [
@@ -6871,6 +6871,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Stress: Error Rate",
             "value": 0.018,
+            "unit": "%"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "astandrik@yandex-team.ru",
+            "name": "Anton Standrik",
+            "username": "astandrik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "39d8d1c6231f133692e7b574de54f783f3778ecd",
+          "message": "fix: add transient checks, idempotent and cached flags (#131)",
+          "timestamp": "2025-12-05T17:46:23+03:00",
+          "tree_id": "8355561cf5ffc0ff22a1b43321d6fb0a6182cf66",
+          "url": "https://github.com/astandrik/ydb-qdrant/commit/39d8d1c6231f133692e7b574de54f783f3778ecd"
+        },
+        "date": 1764946452847,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Stress: Search Latency p95",
+            "value": 5443,
+            "unit": "ms"
+          },
+          {
+            "name": "Stress: Search Latency p99",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "Stress: Search Latency max",
+            "value": 5722,
+            "unit": "ms"
+          },
+          {
+            "name": "Stress: Upsert Latency p95",
+            "value": 4400,
+            "unit": "ms"
+          },
+          {
+            "name": "Stress: Error Rate",
+            "value": 0,
             "unit": "%"
           }
         ]
