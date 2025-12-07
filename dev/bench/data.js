03b0dd68df23f824815a2ce19d76f969b2433322
github-action-benchmark <github@users.noreply.github.com>
2025-12-07 13:00:23 +0000

    add Load Test - Stress (one_table, 768D) (customSmallerIsBetter) benchmark result for e161aad9e9a5681f9837766733f657d97c5f8ffe

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1765112421012,
+  "lastUpdate": 1765112423901,
   "repoUrl": "https://github.com/astandrik/ydb-qdrant",
   "entries": {
     "Load Test - Soak (multi_table, 768D)": [
@@ -8584,6 +8584,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Stress: Error Rate",
             "value": 0.4312,
+            "unit": "%"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "astandrik@yandex-team.ru",
+            "name": "Anton Standrik",
+            "username": "astandrik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e161aad9e9a5681f9837766733f657d97c5f8ffe",
+          "message": "chore(main): release 5.1.0 (#139)",
+          "timestamp": "2025-12-07T15:52:31+03:00",
+          "tree_id": "de74675e5cd561abb65d7194934b156a4daf3dc9",
+          "url": "https://github.com/astandrik/ydb-qdrant/commit/e161aad9e9a5681f9837766733f657d97c5f8ffe"
+        },
+        "date": 1765112422953,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Stress: Search Latency p95",
+            "value": 4505,
+            "unit": "ms"
+          },
+          {
+            "name": "Stress: Search Latency p99",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "Stress: Search Latency max",
+            "value": 8497,
+            "unit": "ms"
+          },
+          {
+            "name": "Stress: Upsert Latency p95",
+            "value": 3918.5,
+            "unit": "ms"
+          },
+          {
+            "name": "Stress: Error Rate",
+            "value": 0.022,
             "unit": "%"
           }
         ]
