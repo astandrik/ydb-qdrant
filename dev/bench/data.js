8fa0ba6db2598371e66cf0855a90ef2929b4a2c5
github-action-benchmark <github@users.noreply.github.com>
2025-12-07 08:52:03 +0000

    add Load Test - Soak (one_table, 768D) (customSmallerIsBetter) benchmark result for a568d7fc57f3e749715e5e115947a16ba659f5c8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1765097513842,
+  "lastUpdate": 1765097523455,
   "repoUrl": "https://github.com/astandrik/ydb-qdrant",
   "entries": {
     "Load Test - Soak (multi_table, 768D)": [
@@ -3194,6 +3194,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "Soak: Error Rate",
             "value": 0,
+            "unit": "%"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "astandrik@yandex-team.ru",
+            "name": "Anton Standrik",
+            "username": "astandrik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a568d7fc57f3e749715e5e115947a16ba659f5c8",
+          "message": "feat: autopartition table (#137)",
+          "timestamp": "2025-12-07T11:48:05+03:00",
+          "tree_id": "d32a65fe96a355bae15623039b9caa9bb735d382",
+          "url": "https://github.com/astandrik/ydb-qdrant/commit/a568d7fc57f3e749715e5e115947a16ba659f5c8"
+        },
+        "date": 1765097522561,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Soak: Search Latency p95",
+            "value": 70,
+            "unit": "ms"
+          },
+          {
+            "name": "Soak: Search Latency p99",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "Soak: Upsert Latency p95",
+            "value": 23,
+            "unit": "ms"
+          },
+          {
+            "name": "Soak: Error Rate",
+            "value": 0.0278,
             "unit": "%"
           }
         ]
