c67d7ead83fe637ca4b46ef9d63f82165092187f
github-action-benchmark <github@users.noreply.github.com>
2025-12-07 08:06:56 +0000

    add Load Test - Stress Capacity (one_table, 768D) (customBiggerIsBetter) benchmark result for d7bcd03f6bc0782f61cc06b49950584325793116

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1765094815163,
+  "lastUpdate": 1765094816171,
   "repoUrl": "https://github.com/astandrik/ydb-qdrant",
   "entries": {
     "Load Test - Soak (multi_table, 768D)": [
@@ -8965,6 +8965,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "Stress: Throughput",
             "value": 153.11355515416815,
+            "unit": "ops/s"
+          },
+          {
+            "name": "Stress: Max VUs",
+            "value": 600,
+            "unit": "VUs"
+          },
+          {
+            "name": "Stress: Breaking Point VUs",
+            "value": 600,
+            "unit": "VUs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "astandrik@yandex-team.ru",
+            "name": "Anton Standrik",
+            "username": "astandrik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d7bcd03f6bc0782f61cc06b49950584325793116",
+          "message": "feat!:  leave only one-table mode (#135)\n\n* feat!:  leave only one-table mode\n\n* fix: fix typecheck\n\n* fix: docs\n\n* fix: docs\n\n* fix: remove legacy\n\n* fix: add startup probe",
+          "timestamp": "2025-12-07T10:59:01+03:00",
+          "tree_id": "e19c12bf009aad531ceaae394ffa5aaf606c730b",
+          "url": "https://github.com/astandrik/ydb-qdrant/commit/d7bcd03f6bc0782f61cc06b49950584325793116"
+        },
+        "date": 1765094815869,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Stress: Throughput",
+            "value": 153.77079741451044,
             "unit": "ops/s"
           },
           {
