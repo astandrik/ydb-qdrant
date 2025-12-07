a28d3726e859e87dae91200fad3aba166f8ac4d1
github-action-benchmark <github@users.noreply.github.com>
2025-12-07 08:02:54 +0000

    add Load Test - Soak (one_table, 768D) (customSmallerIsBetter) benchmark result for d7bcd03f6bc0782f61cc06b49950584325793116

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1764948006695,
+  "lastUpdate": 1765094574772,
   "repoUrl": "https://github.com/astandrik/ydb-qdrant",
   "entries": {
     "Load Test - Soak (multi_table, 768D)": [
@@ -2969,6 +2969,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "Soak: Upsert Latency p95",
             "value": 23,
+            "unit": "ms"
+          },
+          {
+            "name": "Soak: Error Rate",
+            "value": 0,
+            "unit": "%"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "astandrik@yandex-team.ru",
+            "name": "Anton Standrik",
+            "username": "astandrik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d7bcd03f6bc0782f61cc06b49950584325793116",
+          "message": "feat!:  leave only one-table mode (#135)\n\n* feat!:  leave only one-table mode\n\n* fix: fix typecheck\n\n* fix: docs\n\n* fix: docs\n\n* fix: remove legacy\n\n* fix: add startup probe",
+          "timestamp": "2025-12-07T10:59:01+03:00",
+          "tree_id": "e19c12bf009aad531ceaae394ffa5aaf606c730b",
+          "url": "https://github.com/astandrik/ydb-qdrant/commit/d7bcd03f6bc0782f61cc06b49950584325793116"
+        },
+        "date": 1765094573760,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Soak: Search Latency p95",
+            "value": 96,
+            "unit": "ms"
+          },
+          {
+            "name": "Soak: Search Latency p99",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "Soak: Upsert Latency p95",
+            "value": 21,
             "unit": "ms"
           },
           {
