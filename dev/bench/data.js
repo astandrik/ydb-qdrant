fe7f90124db148f9be8d14fa5f809152edc24f8a
github-action-benchmark <github@users.noreply.github.com>
2025-12-08 22:37:22 +0000

    add Load Test - Soak Capacity (one_table, 768D) (customBiggerIsBetter) benchmark result for 8fa9715cd06cdf7f83956d07b7d5c7b8aca8b9c4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1765233441109,
+  "lastUpdate": 1765233442768,
   "repoUrl": "https://github.com/astandrik/ydb-qdrant",
   "entries": {
     "Load Test - Soak (multi_table, 768D)": [
@@ -5597,6 +5597,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Soak: Throughput",
             "value": 71.83513782486912,
+            "unit": "ops/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "astandrik@yandex-team.ru",
+            "name": "Anton Standrik",
+            "username": "astandrik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8fa9715cd06cdf7f83956d07b7d5c7b8aca8b9c4",
+          "message": "chore(main): release 5.2.0 (#142)",
+          "timestamp": "2025-12-09T01:33:29+03:00",
+          "tree_id": "bce6e17c86a1a3164fa7cd85d89dee86f02fd274",
+          "url": "https://github.com/astandrik/ydb-qdrant/commit/8fa9715cd06cdf7f83956d07b7d5c7b8aca8b9c4"
+        },
+        "date": 1765233442451,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Soak: Throughput",
+            "value": 64.66848787764548,
             "unit": "ops/s"
           }
         ]
