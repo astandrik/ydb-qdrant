9be4eeeeb995e2e0336026a2addc0b9bac7e6e1d
github-action-benchmark <github@users.noreply.github.com>
2025-12-10 15:18:23 +0000

    add Load Test - Soak (one_table, 768D) (customSmallerIsBetter) benchmark result for cd73c1e8c5de2313353bf146ffd5d2dc1c0da380

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1765379902387,
+  "lastUpdate": 1765379903680,
   "repoUrl": "https://github.com/astandrik/ydb-qdrant",
   "entries": {
     "Load Test - Soak (multi_table, 768D)": [
@@ -4250,6 +4250,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "Soak: Error Rate",
             "value": 0,
+            "unit": "%"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "astandrik@yandex-team.ru",
+            "name": "Anton Standrik",
+            "username": "astandrik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "cd73c1e8c5de2313353bf146ffd5d2dc1c0da380",
+          "message": "feat!: add collection last access timing (#157)\n\n* feat!: add collection last access timing\n\n* fix: review fixes",
+          "timestamp": "2025-12-10T18:14:32+03:00",
+          "tree_id": "e84daddb2bbee6f53c09651e1f7fc8d221d299cc",
+          "url": "https://github.com/astandrik/ydb-qdrant/commit/cd73c1e8c5de2313353bf146ffd5d2dc1c0da380"
+        },
+        "date": 1765379902473,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Soak: Search Latency p95",
+            "value": 71,
+            "unit": "ms"
+          },
+          {
+            "name": "Soak: Search Latency p99",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "Soak: Upsert Latency p95",
+            "value": 26,
+            "unit": "ms"
+          },
+          {
+            "name": "Soak: Error Rate",
+            "value": 0.0094,
             "unit": "%"
           }
         ]
