68f52bce6e4752c96af57d4ca7b5cb8a6081a2f2
github-action-benchmark <github@users.noreply.github.com>
2025-12-23 13:56:28 +0000

    add Load Test - Soak (one_table, 768D) (customSmallerIsBetter) benchmark result for 7c3268eb49ce6809f9058a890fa09af06e559c97

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1766498187013,
+  "lastUpdate": 1766498188233,
   "repoUrl": "https://github.com/astandrik/ydb-qdrant",
   "entries": {
     "Load Test - Soak (multi_table, 768D)": [
@@ -4778,6 +4778,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "Soak: Error Rate",
             "value": 0,
+            "unit": "%"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "41898282+github-actions[bot]@users.noreply.github.com",
+            "name": "github-actions[bot]",
+            "username": "github-actions[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7c3268eb49ce6809f9058a890fa09af06e559c97",
+          "message": "chore(main): release 6.0.2 (#162)\n\nCo-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>",
+          "timestamp": "2025-12-23T16:52:37+03:00",
+          "tree_id": "77520611ac59782985b605809e4e03f6f53ab34d",
+          "url": "https://github.com/astandrik/ydb-qdrant/commit/7c3268eb49ce6809f9058a890fa09af06e559c97"
+        },
+        "date": 1766498184990,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Soak: Search Latency p95",
+            "value": 69,
+            "unit": "ms"
+          },
+          {
+            "name": "Soak: Search Latency p99",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "Soak: Upsert Latency p95",
+            "value": 25,
+            "unit": "ms"
+          },
+          {
+            "name": "Soak: Error Rate",
+            "value": 0.0093,
             "unit": "%"
           }
         ]
