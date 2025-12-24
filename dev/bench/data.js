e33644d32e915df9a7317afcb9cd70240798db23
github-action-benchmark <github@users.noreply.github.com>
2025-12-24 19:57:05 +0000

    add Load Test - Stress (one_table, 768D) (customSmallerIsBetter) benchmark result for 43a30d2bde280085ddaaa5fe11add7f6229fe4c1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1766605986634,
+  "lastUpdate": 1766606225007,
   "repoUrl": "https://github.com/astandrik/ydb-qdrant",
   "entries": {
     "Load Test - Soak (multi_table, 768D)": [
@@ -13903,6 +13903,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Stress: Error Rate",
             "value": 0.0427,
+            "unit": "%"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "astandrik@yandex-team.ru",
+            "name": "Anton Standrik",
+            "username": "astandrik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "43a30d2bde280085ddaaa5fe11add7f6229fe4c1",
+          "message": "fix: root route (#168)\n\n* fix: root route\n\n* fix: other errors",
+          "timestamp": "2025-12-24T22:49:18+03:00",
+          "tree_id": "71ab2dedac8c9cfd93a37531d5e69a36a25264b5",
+          "url": "https://github.com/astandrik/ydb-qdrant/commit/43a30d2bde280085ddaaa5fe11add7f6229fe4c1"
+        },
+        "date": 1766606223798,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Stress: Search Latency p95",
+            "value": 3699.199999999999,
+            "unit": "ms"
+          },
+          {
+            "name": "Stress: Search Latency p99",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "Stress: Search Latency max",
+            "value": 10076,
+            "unit": "ms"
+          },
+          {
+            "name": "Stress: Upsert Latency p95",
+            "value": 3412.5999999999985,
+            "unit": "ms"
+          },
+          {
+            "name": "Stress: Error Rate",
+            "value": 0.9794,
             "unit": "%"
           }
         ]
