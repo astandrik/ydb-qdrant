2e065db335e0ce9e6f5badbb7f8e415f23210ece
github-action-benchmark <github@users.noreply.github.com>
2025-12-24 14:21:38 +0000

    add Load Test - Stress (one_table, 768D) (customSmallerIsBetter) benchmark result for 617ba82f84c34ec5b2628a8062dc563fa63e7ea0

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1766586089991,
+  "lastUpdate": 1766586098174,
   "repoUrl": "https://github.com/astandrik/ydb-qdrant",
   "entries": {
     "Load Test - Soak (multi_table, 768D)": [
@@ -13708,6 +13708,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Stress: Error Rate",
             "value": 1.081,
+            "unit": "%"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "41898282+github-actions[bot]@users.noreply.github.com",
+            "name": "github-actions[bot]",
+            "username": "github-actions[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "617ba82f84c34ec5b2628a8062dc563fa63e7ea0",
+          "message": "chore(main): release 6.1.1 (#167)\n\nCo-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>",
+          "timestamp": "2025-12-24T17:13:39+03:00",
+          "tree_id": "b65afd5add3e313d99dcc359b941f7e30abff50a",
+          "url": "https://github.com/astandrik/ydb-qdrant/commit/617ba82f84c34ec5b2628a8062dc563fa63e7ea0"
+        },
+        "date": 1766586097055,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Stress: Search Latency p95",
+            "value": 4552,
+            "unit": "ms"
+          },
+          {
+            "name": "Stress: Search Latency p99",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "Stress: Search Latency max",
+            "value": 5603,
+            "unit": "ms"
+          },
+          {
+            "name": "Stress: Upsert Latency p95",
+            "value": 4145.549999999999,
+            "unit": "ms"
+          },
+          {
+            "name": "Stress: Error Rate",
+            "value": 0.0427,
             "unit": "%"
           }
         ]
