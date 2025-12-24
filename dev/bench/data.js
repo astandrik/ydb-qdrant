390df44c21832616bebf93fb55b4e9e15798d89d
github-action-benchmark <github@users.noreply.github.com>
2025-12-24 14:21:39 +0000

    add Load Test - Stress Capacity (one_table, 768D) (customBiggerIsBetter) benchmark result for 617ba82f84c34ec5b2628a8062dc563fa63e7ea0

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1766586098174,
+  "lastUpdate": 1766586099838,
   "repoUrl": "https://github.com/astandrik/ydb-qdrant",
   "entries": {
     "Load Test - Soak (multi_table, 768D)": [
@@ -17113,6 +17113,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "Stress: Breaking Point VUs",
             "value": -1,
+            "unit": "VUs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "41898282+github-actions[bot]@users.noreply.github.com",
+            "name": "github-actions[bot]",
+            "username": "github-actions[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "617ba82f84c34ec5b2628a8062dc563fa63e7ea0",
+          "message": "chore(main): release 6.1.1 (#167)\n\nCo-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>",
+          "timestamp": "2025-12-24T17:13:39+03:00",
+          "tree_id": "b65afd5add3e313d99dcc359b941f7e30abff50a",
+          "url": "https://github.com/astandrik/ydb-qdrant/commit/617ba82f84c34ec5b2628a8062dc563fa63e7ea0"
+        },
+        "date": 1766586099198,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Stress: Throughput",
+            "value": 169.07668278625374,
+            "unit": "ops/s"
+          },
+          {
+            "name": "Stress: Max VUs",
+            "value": 600,
+            "unit": "VUs"
+          },
+          {
+            "name": "Stress: Breaking Point VUs",
+            "value": 600,
             "unit": "VUs"
           }
         ]
