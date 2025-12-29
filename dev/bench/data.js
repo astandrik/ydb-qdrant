7d1a22e48d45d5053d38c54d749917607b339fb8
github-action-benchmark <github@users.noreply.github.com>
2025-12-29 14:50:06 +0000

    add Load Test - Soak Capacity (one_table, 768D) (customBiggerIsBetter) benchmark result for a8fcf28ac42ccc4ab5be87200d73d708f12244c1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1767019804929,
+  "lastUpdate": 1767019806475,
   "repoUrl": "https://github.com/astandrik/ydb-qdrant",
   "entries": {
     "Load Test - Soak (one_table, 768D)": [
@@ -5009,6 +5009,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Soak: Throughput",
             "value": 71.98778040197985,
+            "unit": "ops/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "41898282+github-actions[bot]@users.noreply.github.com",
+            "name": "github-actions[bot]",
+            "username": "github-actions[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a8fcf28ac42ccc4ab5be87200d73d708f12244c1",
+          "message": "chore(main): release 7.0.1 (#179)\n\nCo-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>",
+          "timestamp": "2025-12-29T17:46:21+03:00",
+          "tree_id": "3b42275ce4fe640133960e0c696a7450267f20d7",
+          "url": "https://github.com/astandrik/ydb-qdrant/commit/a8fcf28ac42ccc4ab5be87200d73d708f12244c1"
+        },
+        "date": 1767019805872,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Soak: Throughput",
+            "value": 72.37493138195197,
             "unit": "ops/s"
           }
         ]
