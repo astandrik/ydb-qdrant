4ef014bdbcab102eff268249fc335e940d57a37b
github-action-benchmark <github@users.noreply.github.com>
2025-12-29 14:54:02 +0000

    add Load Test - Stress Capacity (one_table, 768D) (customBiggerIsBetter) benchmark result for a8fcf28ac42ccc4ab5be87200d73d708f12244c1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1767020041722,
+  "lastUpdate": 1767020042736,
   "repoUrl": "https://github.com/astandrik/ydb-qdrant",
   "entries": {
     "Load Test - Soak (one_table, 768D)": [
@@ -11138,6 +11138,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "Stress: Throughput",
             "value": 221.864738472457,
+            "unit": "ops/s"
+          },
+          {
+            "name": "Stress: Max VUs",
+            "value": 600,
+            "unit": "VUs"
+          },
+          {
+            "name": "Stress: Breaking Point VUs",
+            "value": 600,
+            "unit": "VUs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "41898282+github-actions[bot]@users.noreply.github.com",
+            "name": "github-actions[bot]",
+            "username": "github-actions[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a8fcf28ac42ccc4ab5be87200d73d708f12244c1",
+          "message": "chore(main): release 7.0.1 (#179)\n\nCo-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>",
+          "timestamp": "2025-12-29T17:46:21+03:00",
+          "tree_id": "3b42275ce4fe640133960e0c696a7450267f20d7",
+          "url": "https://github.com/astandrik/ydb-qdrant/commit/a8fcf28ac42ccc4ab5be87200d73d708f12244c1"
+        },
+        "date": 1767020042491,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Stress: Throughput",
+            "value": 218.4214798612896,
             "unit": "ops/s"
           },
           {
