b6fac62569d9568cebc6114ebce7e175d8d49380
github-action-benchmark <github@users.noreply.github.com>
2026-06-29 10:38:46 +0000

    add Load Test - Soak Capacity (one_table exact, 768D) (customBiggerIsBetter) benchmark result for 41f43e1c061e861b0d123c768e21faf823e17783

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1782729524884,
+  "lastUpdate": 1782729526807,
   "repoUrl": "https://github.com/astandrik/ydb-qdrant",
   "entries": {
     "Load Test - Soak (one_table, 768D)": [
@@ -14350,6 +14350,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Soak: Throughput",
             "value": 58.67091462232581,
+            "unit": "ops/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "astandrik@yandex-team.ru",
+            "name": "Anton Standrik",
+            "username": "astandrik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "41f43e1c061e861b0d123c768e21faf823e17783",
+          "message": "refactor: remove legacy one-table naming (#202)",
+          "timestamp": "2026-06-29T13:34:52+03:00",
+          "tree_id": "7272604b72e72a82beef018222eae0fa8499ab93",
+          "url": "https://github.com/astandrik/ydb-qdrant/commit/41f43e1c061e861b0d123c768e21faf823e17783"
+        },
+        "date": 1782729526225,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Soak: Throughput",
+            "value": 66.52285259434893,
             "unit": "ops/s"
           }
         ]
